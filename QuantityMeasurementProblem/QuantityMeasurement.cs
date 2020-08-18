079f30eccd17b800a90cc7ba7728180dd2918715
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-08-18 20:49:04 +0530

    [Akshay]UC1:ADDED:Test Case And Code For Null Check

--- QuantityMeasurementProblem/QuantityMeasurement.cs
+++ QuantityMeasurementProblem/QuantityMeasurement.cs
@@ -1,9 +1,43 @@
-﻿using System;
+﻿// <copyright file="QuantityMeasurement.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace QuantityMeasurementProblem
 {
+    /// <summary>
+    /// Main Class.
+    /// </summary>
     public class QuantityMeasurement
     {
+        /// <summary>
+        /// Method to return feet.
+        /// </summary>
+        /// <param name="feet"></param>
+        /// <returns>Feet value.</returns>
         public double ReturnFeet(double feet) => feet;
+
+        /// <summary>
+        /// Overridding Equals Method.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns> boolean. </returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Override Hashcode function.
+        /// </summary>
+        /// <returns> returns hash code for current object.</returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
--- QuantityMeasurementTest/QuantityMeasurementTest.cs
+++ QuantityMeasurementTest/QuantityMeasurementTest.cs
@@ -1,34 +1,61 @@
-using NUnit.Framework;
-using QuantityMeasurementProblem;
+// <copyright file="QuantityMeasurementTest.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace QuantityMeasurementTest
 {
+    using NUnit.Framework;
+    using QuantityMeasurementProblem;
+
+    /// <summary>
+    /// Test Class.
+    /// </summary>
     public class QuantityMeasurementTest
     {
-        QuantityMeasurement quantityMeasurement; 
+        /// <summary>
+        /// Creating Instance Of Main Class.
+        /// </summary>
+        private QuantityMeasurement quantityMeasurement;
 
-
+        /// <summary>
+        /// Set up for tests.
+        /// </summary>
         [SetUp]
         public void Setup()
         {
-            quantityMeasurement = new QuantityMeasurement();
-
+            this.quantityMeasurement = new QuantityMeasurement();
         }
 
+        /// <summary>
+        /// Test case to equate zero.
+        /// </summary>
         [Test]
         public void GivenZeroFeetAndZeroFeet_IfEqual_ThenShouldReturnTrue()
         {
-            double feetValueOne = quantityMeasurement.ReturnFeet(0.0);
-            double feetValueTwo = quantityMeasurement.ReturnFeet(0.0);
+            double feetValueOne = this.quantityMeasurement.ReturnFeet(0.0);
+            double feetValueTwo = this.quantityMeasurement.ReturnFeet(0.0);
             Assert.AreEqual(feetValueOne, feetValueTwo);
         }
 
+        /// <summary>
+        /// Test case to equate zero and One.
+        /// </summary>
         [Test]
-        public void GivenDifferentFeetValues_IfNotEqual_ShouldReturnTrue()
+        public void GivenDifferentFeetValues_IfNotEqual_ThenShouldReturnTrue()
         {
-            double feetValueOne = quantityMeasurement.ReturnFeet(0.0);
-            double feetValueTwo = quantityMeasurement.ReturnFeet(1.0);
+            double feetValueOne = this.quantityMeasurement.ReturnFeet(0.0);
+            double feetValueTwo = this.quantityMeasurement.ReturnFeet(1.0);
             Assert.AreNotEqual(feetValueOne, feetValueTwo);
+        }
+
+        /// <summary>
+        /// Test case to equate null.
+        /// </summary>
+        [Test]
+        public void GivenNullValueForFeet_IfEqual_ThenShouldReturnFalse()
+        {
+            bool equals = this.quantityMeasurement.Equals(null);
+            Assert.IsFalse(equals);
         }
     }
 }
